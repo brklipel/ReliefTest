b198e7a0d8a7a5150a4b8fa14fe7e7eb90ae75e8
brklipel <30844487+brklipel@users.noreply.github.com>
2018-07-13 12:03:34 -0300

    Update PlayFabGettingStarted.js

--- PlayFabGettingStarted.js
+++ PlayFabGettingStarted.js
@@ -1,19 +1,6 @@
-function DoExampleLoginWithCustomID(){
-    PlayFab.settings.titleId = document.getElementById("titleId").value;
-    var loginRequest = {
-        // Currently, you need to look up the correct format for this object in the API-docs:
-        // https://api.playfab.com/documentation/Client/method/LoginWithCustomID
-        TitleId: PlayFab.settings.titleId,
-        CustomId: document.getElementById("customId").value,
-        CreateAccount: true
-    };
-
-    PlayFabClientSDK.LoginWithCustomID(loginRequest, LoginCallback);
-}
-
-var LoginCallback = function (result, error) {
+var GetLeaderboardCallback = function (result, error) {
     if (result !== null) {
-        document.getElementById("resultOutput").innerHTML = "Congratulations, you made your first successful API call!";
+        document.getElementById("resultOutput").innerHTML = result.status;
     } else if (error !== null) {
         document.getElementById("resultOutput").innerHTML =
             "Something went wrong with your first API call.\n" +
@@ -24,15 +11,11 @@ var LoginCallback = function (result, error) {
 
 function DoExampleGetLeaderboard(){
     PlayFab.settings.titleId = document.getElementById("titleId").value;
-    //PlayFab.settings.developerSecretKey = "A61W9RSK9O3TDDBTUES7DTRBWYG3U317HJ4OB3PH51Q3IFSCB7";
     var getLeaderboardRequest = {
-        // Currently, you need to look up the correct format for this object in the API-docs:
-        // https://api.playfab.com/documentation/Client/method/LoginWithCustomID
-        //TitleId: PlayFab.settings.titleId,
 		StatisticName: "score",
 		StartPosition: 0,
 		MaxResultsCount: 20
     };
 
-    PlayFabServerSDK.GetLeaderboard(getLeaderboardRequest, LoginCallback);
+    PlayFabServerSDK.GetLeaderboard(getLeaderboardRequest, GetLeaderboardCallback);
 }
