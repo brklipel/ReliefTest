6a1e8bee41bb652dfcfeec6d2a0216d78e8be3b3
brklipel <30844487+brklipel@users.noreply.github.com>
2018-07-16 10:21:23 -0300

    Update PlayFabGettingStarted.js

--- PlayFabGettingStarted.js
+++ PlayFabGettingStarted.js
@@ -1,7 +1,27 @@
 var GetLeaderboardCallback = function (result, error) {
-    if (result !== null) {
-        document.getElementById("resultOutput").innerHTML = result.status;
-    } else if (error !== null) {
+    if (result !== null) 
+	{
+		var wrapper = document.getElementById("myHTMLWrapper");
+		var myHTML = '';
+		
+		var players = {};
+		players = result.data;
+		
+		var __string = "";
+		
+		__string += "<tr><th>Position</th><th>Name</th><th>Score</th></tr>"
+		var i = 0;
+		for(i = 0; i < players.Leaderboard.length; i++)
+		{
+			var __player = players.Leaderboard[i];
+			
+			__string += "<tr>" + "<td>" + (__player.Position + 1) + "</td>" + "<td>" + __player.DisplayName + "</td>" + "<td>" + __player.StatValue + "</td></tr>";			
+		}
+		
+		myHTML += '<span class="test"><table border="1" width="90%" align="center">' + __string +'</table></span><br/><br/>';
+		wrapper.innerHTML = myHTML;
+    } 
+	else if (error !== null) {
         document.getElementById("resultOutput").innerHTML =
             "Something went wrong with your first API call.\n" +
             "Here's some debug information:\n" +
@@ -15,7 +35,7 @@ function DoExampleGetLeaderboard(){
     var getLeaderboardRequest = {
 		StatisticName: "score",
 		StartPosition: 0,
-		MaxResultsCount: 20
+		MaxResultsCount: 100
     };
 
     PlayFabServerSDK.GetLeaderboard(getLeaderboardRequest, GetLeaderboardCallback);
