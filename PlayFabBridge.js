52940580f22c411ff132b067085b12cd848b69a1
brklipel <30844487+brklipel@users.noreply.github.com>
2018-07-16 11:21:15 -0300

    Add files via upload

---
+++ PlayFabBridge.js
@@ -0,0 +1,52 @@
+var GetLeaderboardCallback = function (result, error) {
+    if (result !== null) 
+	{
+		var wrapper = document.getElementById("myHTMLWrapper");
+		var myHTML = '';
+		
+		var players = {};
+		players = result.data;
+		
+		var __string = "";
+			
+		__string += "<tr><th>Position</th><th>Name</th><th>Score</th></tr>"
+		var i = 0;
+		for(i = 0; i < players.Leaderboard.length; i++)
+		{
+			var __player = players.Leaderboard[i];
+			
+			__string += "<tr>" + "<td>" + (__player.Position + 1) + "</td>" + "<td>" + __player.DisplayName + "</td>" + "<td>" + __player.StatValue + "</td></tr>";			
+		}
+		
+		var d = new Date();
+		var n = d.getTime();
+
+		myHTML += '<span class="test"><div align="center">'+ n +'</div><br/><table border="1" width="90%" align="center">' + __string +'</table></span><br/><br/>';		
+		wrapper.innerHTML = myHTML;			
+    } 
+	else if (error !== null) {
+        document.getElementById("resultOutput").innerHTML =
+            "Something went wrong with your first API call.\n" +
+            "Here's some debug information:\n" +
+            PlayFab.GenerateErrorReport(error);
+    }
+}
+
+function StartLoad()
+{
+	DoExampleGetLeaderboard();
+	
+	setInterval(DoExampleGetLeaderboard(), 5000);
+}
+
+function DoExampleGetLeaderboard(){
+    PlayFab.settings.titleId = "BE5B";
+	PlayFab.settings.developerSecretKey = "A61W9RSK9O3TDDBTUES7DTRBWYG3U317HJ4OB3PH51Q3IFSCB7";
+    var getLeaderboardRequest = {
+		StatisticName: "score",
+		StartPosition: 0,
+		MaxResultsCount: 100
+    };
+
+    PlayFabServerSDK.GetLeaderboard(getLeaderboardRequest, GetLeaderboardCallback);
+}
