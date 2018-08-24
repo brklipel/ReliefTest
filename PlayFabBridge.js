4646f9df2007f8ec851dcf54d73a9cdbdb311ac8
brklipel <30844487+brklipel@users.noreply.github.com>
2018-08-24 16:36:57 -0300

    Add files via upload

--- PlayFabBridge.js
+++ PlayFabBridge.js
@@ -9,13 +9,13 @@ var GetLeaderboardCallback = function (result, error) {
 		
 		var __string = "";
 		
-		__string += "<tr><th background="+"BarBlue.png"+">Position</th><th background="+"BarGreen.png"+">Name</th><th background="+"BarRed.png"+">Score</th></tr>"
+		__string += "<tr><th background="+"BarBlue.png"+"><FONT size="+"5"+" COLOR="+"FFFFFF"+">Position</th><th background="+"BarGreen.png"+"><FONT size="+"5"+" COLOR="+"FFFFFF"+">Name</th><th background="+"BarRed.png"+"><FONT size="+"5"+" COLOR="+"FFFFFF"+">Score</th></tr>"
 		var i = 0;
 		for(i = 0; i < players.Leaderboard.length; i++)
 		{
 			var __player = players.Leaderboard[i];
 			
-			__string += "<tr>" + "<td align="+"center"+" background="+"BarBlue.png"+">" + (__player.Position + 1) + "</td>" + "<td align="+"center"+" background="+"BarGreen.png"+">" + __player.DisplayName + "</td>" + "<td align="+"center"+" background="+"BarRed.png"+">" + __player.StatValue + "</td></tr>";			
+			__string += "<tr>" + "<td align="+"center"+" background="+"BarBlue.png"+"><font color="+"FFFFFF"+">" + (__player.Position + 1) + "</td>" + "<td align="+"center"+" background="+"BarGreen.png"+"><FONT COLOR="+"FFFFFF"+">" + __player.DisplayName + "</td>" + "<td align="+"center"+" background="+"BarRed.png"+"><FONT COLOR="+"FFFFFF"+">" + __player.StatValue + "</td></tr>";			
 		}
 		
 		myHTML += '<span class="test"><br/><div align="center"><table width="90%" align="center"><img src="Bar.png" width="89.7%">' + __string +'</table></div></span><br/><br/>';		
